e23f50c4e744ac3ad47e4b8e767bb574e9895304
chltkdwns <137965230+chltkdwns@users.noreply.github.com>
2025-08-06 16:25:02 +0900

    .

---
+++ src/main/java/org/koreait/global/entities/BaseEntity.java
@@ -0,0 +1,28 @@
+package org.koreait.global.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime modifiedAt;
+
+
+    private LocalDateTime deletedAt;
+}
